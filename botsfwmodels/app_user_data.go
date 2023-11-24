25318895c5e203dd9bb5cc642055a7f644a9ade2
Alexander Trakhimenok <533159+trakhimenok@users.noreply.github.com>
2023-11-24 20:05:01 +0000

    feat(deps): adds dep on github.com/strongo/strongoapp

--- botsfwmodels/app_user_data.go
+++ botsfwmodels/app_user_data.go
@@ -1,13 +1,13 @@
 package botsfwmodels
 
+import "github.com/strongo/strongoapp/appuser"
+
 // AppUserData holds information about bot app user
 type AppUserData interface {
+	appuser.BaseUserData
 
 	// LocaleSettings is an interface to set and get preferred locale
 	LocaleSettings
-
-	// SetNames sets user record name fields using information provided by bot platform
-	SetNames(first, last string)
 
 	// SetBotUserID associates bot user ID with an app user record
 	SetBotUserID(platform, botID, botUserID string)
