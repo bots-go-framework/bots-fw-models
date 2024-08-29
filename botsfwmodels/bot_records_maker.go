535a66beb1b9b048d3c8c921bf8ce2af3e92783d
Alexander Trakhimenok <533159+trakhimenok@users.noreply.github.com>
2024-08-29 17:28:59 +0100

    feat: removed deprecated interface BotRecordsMaker

--- botsfwmodels/bot_records_maker.go
+++ botsfwmodels/bot_records_maker.go
@@ -3,61 +3,61 @@ package botsfwmodels
 // BotRecordsMaker is an interface for making bot records
 // This should be implemented by platform adapters
 // (for example, by https://github.com/bots-go-framework/bots-fw-telegram)
-type BotRecordsMaker interface {
-	Platform() string
-
-	// MakeAppUserDto makes app user DTO for a given request sender
-	MakeAppUserDto(botID string) (AppUserData, error)
-
-	// MakeBotUserDto makes bot user DTO for a given request sender
-	MakeBotUserDto(botID string) (PlatformUserData, error)
-
-	// MakeBotChatDto makes bot chat DTO for a given request
-	MakeBotChatDto(botID string) (BotChatData, error)
-}
-
-func NewBotRecordsMaker(
-	platform string,
-	makeAppUserDto func(botID string) (appUser AppUserData, err error),
-	makeBotUserDto func(botID string) (botUser PlatformUserData, err error),
-	makeBotChatDto func(botID string) (botChat BotChatData, err error),
-) BotRecordsMaker {
-	if makeAppUserDto == nil {
-		panic("makeAppUserDto is nil")
-	}
-	if makeBotUserDto == nil {
-		panic("makeBotUserDto is nil")
-	}
-	if makeBotChatDto == nil {
-		panic("makeBotChatDto is nil")
-	}
-	return botRecordsMaker{
-		platform:       platform,
-		makeAppUserDto: makeAppUserDto,
-		makeBotUserDto: makeBotUserDto,
-		makeBotChatDto: makeBotChatDto,
-	}
-}
-
-type botRecordsMaker struct {
-	platform       string
-	makeAppUserDto func(botID string) (AppUserData, error)
-	makeBotUserDto func(botID string) (PlatformUserData, error)
-	makeBotChatDto func(botID string) (botChat BotChatData, err error)
-}
-
-func (b botRecordsMaker) Platform() string {
-	return b.platform
-}
-
-func (b botRecordsMaker) MakeAppUserDto(botID string) (AppUserData, error) {
-	return b.makeAppUserDto(botID)
-}
-
-func (b botRecordsMaker) MakeBotUserDto(botID string) (PlatformUserData, error) {
-	return b.makeBotUserDto(botID)
-}
-
-func (b botRecordsMaker) MakeBotChatDto(botID string) (botChat BotChatData, err error) {
-	return b.makeBotChatDto(botID)
-}
+//type BotRecordsMaker interface {
+//	Platform() string
+//
+//	// MakeAppUserDto makes app user DTO for a given request sender
+//	MakeAppUserDto(botID string) (AppUserData, error)
+//
+//	// MakeBotUserDto makes bot user DTO for a given request sender
+//	MakeBotUserDto(botID string) (PlatformUserData, error)
+//
+//	// MakeBotChatDto makes bot chat DTO for a given request
+//	MakeBotChatDto(botID string) (BotChatData, error)
+//}
+//
+//func NewBotRecordsMaker(
+//	platform string,
+//	makeAppUserDto func(botID string) (appUser AppUserData, err error),
+//	makeBotUserDto func(botID string) (botUser PlatformUserData, err error),
+//	makeBotChatDto func(botID string) (botChat BotChatData, err error),
+//) BotRecordsMaker {
+//	if makeAppUserDto == nil {
+//		panic("makeAppUserDto is nil")
+//	}
+//	if makeBotUserDto == nil {
+//		panic("makeBotUserDto is nil")
+//	}
+//	if makeBotChatDto == nil {
+//		panic("makeBotChatDto is nil")
+//	}
+//	return botRecordsMaker{
+//		platform:       platform,
+//		makeAppUserDto: makeAppUserDto,
+//		makeBotUserDto: makeBotUserDto,
+//		makeBotChatDto: makeBotChatDto,
+//	}
+//}
+//
+//type botRecordsMaker struct {
+//	platform       string
+//	makeAppUserDto func(botID string) (AppUserData, error)
+//	makeBotUserDto func(botID string) (PlatformUserData, error)
+//	makeBotChatDto func(botID string) (botChat BotChatData, err error)
+//}
+//
+//func (b botRecordsMaker) Platform() string {
+//	return b.platform
+//}
+//
+//func (b botRecordsMaker) MakeAppUserDto(botID string) (AppUserData, error) {
+//	return b.makeAppUserDto(botID)
+//}
+//
+//func (b botRecordsMaker) MakeBotUserDto(botID string) (PlatformUserData, error) {
+//	return b.makeBotUserDto(botID)
+//}
+//
+//func (b botRecordsMaker) MakeBotChatDto(botID string) (botChat BotChatData, err error) {
+//	return b.makeBotChatDto(botID)
+//}
